8b7e0700d41dead220f5d303f3ad7ccbb98e7908
MohiniHowal <mohini_1089@yahoo.co.in>
2022-11-26 18:12:58 +0530

    1 file modified, 4 added

---
+++ src/Movies/Movies.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MoviesData from './MoviesData.json';
+function Movies() {
+  const DisplayData = MoviesData.results.map((info) => {
+    return (
+      <tr>
+        <td>EPISODE {info.episode_id}</td>
+        <td>Episode - {info.title}</td>
+        <td>{info.release_date}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table class="table table-striped">
+        <thead></thead>
+        <tbody>{DisplayData}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default JsonDataDisplay;
