476687b7715ab1efe97a877822239d5bc1a6cdd8
MohiniHowal <mohini_1089@yahoo.co.in>
2022-11-27 02:45:04 +0530

    3 files modified

--- src/MoviesComponent/Movies.js
+++ src/MoviesComponent/Movies.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useEffect, useRef } from 'react';
 import MoviesData from './MoviesData.json';
 import { intToRoman } from './Common.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './Movies.css';
 function Movies() {
   const DisplayData = MoviesData.results.map((info) => {
@@ -17,13 +20,55 @@ function Movies() {
     );
   });
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleMenuOne = () => {
+    // do something
+    setOpen(false);
+  };
+
+  const handleMenuTwo = () => {
+    // do something
+    setOpen(false);
+  };
+
+  const trigger = () => {};
+
   return (
     <div className="container">
-      <div id="header" className="col-md-12">
-        <img
-          src="https://stackblitz.com/files/react-hukszn/github/MohiniHowal/Movies_App/main/src/MoviesComponent/logo.png"
-          alt="logo"
-        />
+      <div className="row">
+        <div className="col-md-12">
+          <Dropdown
+            trigger={<button>Dropdown</button>}
+            menu={[
+              <button onClick={handleMenuOne}>Menu 1</button>,
+              <button onClick={handleMenuTwo}>Menu 2</button>,
+            ]}
+          />
+          <div className="dropdown">
+            {React.cloneElement(trigger, {
+              onClick: handleOpen,
+            })}
+            {open ? (
+              <ul className="menu">
+                {menu.map((menuItem, index) => (
+                  <li key={index} className="menu-item">
+                    {React.cloneElement(menuItem, {
+                      onClick: () => {
+                        menuItem.props.onClick();
+                        setOpen(false);
+                      },
+                    })}
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+        </div>
       </div>
       <table className="table table-striped">
         <thead></thead>
