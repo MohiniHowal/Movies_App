336fb8625a65f54f0757da2a989c08ddcb759707
MohiniHowal <mohini_1089@yahoo.co.in>
2022-11-27 11:24:20 +0530

    3 files modified

--- src/MoviesComponent/Movies.js
+++ src/MoviesComponent/Movies.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
 import MoviesData from './MoviesData.json';
 import { intToRoman } from './Common.js';
@@ -9,7 +9,21 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import './Movies.css';
 
 function Movies() {
-  const options = ['one', 'two', 'three'];
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleMenuOne = () => {
+    // do something
+    setOpen(false);
+  };
+
+  const handleMenuTwo = () => {
+    // do something
+    setOpen(false);
+  };
 
   const DisplayData = MoviesData.results.map((info) => {
     let episodeId = intToRoman(info.episode_id);
@@ -28,7 +42,17 @@ function Movies() {
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <Dropdown options={options} value="" placeholder="Select an option" />
+          <button onClick={handleOpen}>Dropdown</button>
+          {open ? (
+            <ul className="menu">
+              <li className="menu-item">
+                <button onClick={handleMenuOne}>Menu 1</button>
+              </li>
+              <li className="menu-item">
+                <button onClick={handleMenuTwo}>Menu 2</button>
+              </li>
+            </ul>
+          ) : null}
         </div>
       </div>
       <table className="table table-striped">
