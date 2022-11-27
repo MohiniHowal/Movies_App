f64e0358716cc512711f82241e6aca9ca3c1d077
MohiniHowal <mohini_1089@yahoo.co.in>
2022-11-27 11:14:21 +0530

    3 files modified

--- src/MoviesComponent/Movies.js
+++ src/MoviesComponent/Movies.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import MoviesData from './MoviesData.json';
 import { intToRoman } from './Common.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Dropdown from 'react-bootstrap/Dropdown';
 import './Movies.css';
+
 function Movies() {
+  const options = ['one', 'two', 'three'];
+
   const DisplayData = MoviesData.results.map((info) => {
     let episodeId = intToRoman(info.episode_id);
     return (
@@ -20,54 +24,11 @@ function Movies() {
     );
   });
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(!open);
-  };
-
-  const handleMenuOne = () => {
-    // do something
-    setOpen(false);
-  };
-
-  const handleMenuTwo = () => {
-    // do something
-    setOpen(false);
-  };
-
-  const trigger = () => {};
-
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <Dropdown
-            trigger={<button>Dropdown</button>}
-            menu={[
-              <button onClick={handleMenuOne}>Menu 1</button>,
-              <button onClick={handleMenuTwo}>Menu 2</button>,
-            ]}
-          />
-          <div className="dropdown">
-            {React.cloneElement(trigger, {
-              onClick: handleOpen,
-            })}
-            {open ? (
-              <ul className="menu">
-                {menu.map((menuItem, index) => (
-                  <li key={index} className="menu-item">
-                    {React.cloneElement(menuItem, {
-                      onClick: () => {
-                        menuItem.props.onClick();
-                        setOpen(false);
-                      },
-                    })}
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-          </div>
+          <Dropdown options={options} value="" placeholder="Select an option" />
         </div>
       </div>
       <table className="table table-striped">
